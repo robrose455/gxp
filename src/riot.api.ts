5619ba57cf653c82f6b2e9fbe2b46b3412c62317
Rob Rose <robrose455@gmail.com>
2024-12-03 20:59:30 -0600

    added dynamic url

--- src/riot.api.ts
+++ src/riot.api.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
 import { MatchData, MatchPreview } from './types';
 
+const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : 'https://gxp-api-277f714413c2.herokuapp.com/'
+
 export async function getMatchPreviews(name: string, tag: string): Promise<MatchPreview[]> {
     try {
-    const response = await axios.get(`http://localhost:4000/matchPreviews?name=${name}&tag=${tag}`);
+    const response = await axios.get(`${BASE_URL}/matchPreviews?name=${name}&tag=${tag}`);
     return response.data;
   } catch (error) {
     console.error('Error getting match preview data:', error);
@@ -13,7 +15,7 @@ export async function getMatchPreviews(name: string, tag: string): Promise<Match
 
 export async function getMatchData(id: string, accountId: string): Promise<MatchData> {
   try {
-    const response = await axios.get(`http://localhost:4000/match?id=${id}&accountId=${accountId}`);
+    const response = await axios.get(`${BASE_URL}/match?id=${id}&accountId=${accountId}`);
     return response.data;
   } catch (error) {
     console.error('Error getting match data:', error);
